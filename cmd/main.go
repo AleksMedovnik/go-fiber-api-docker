687aa1fbdfde740d0bba3c0de159e2c62ea40311
Alex <medovnikaleksander@yandex.ru>
2023-11-27 15:30:52 +0300

    First commit

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go-fiber-api-docker/pkg/common/config"
+	"go-fiber-api-docker/pkg/common/db"
+	"go-fiber-api-docker/pkg/products"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(&c)
+	app := fiber.New()
+
+	products.RegisterRoutes(app, h)
+
+	app.Listen(c.Port)
+}
